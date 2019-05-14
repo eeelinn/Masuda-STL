8640f9d30e9b8fb179321caaf387c13336053f70
Erikka <eeelinn@gmail.com>
2019-05-14 08:40:54 -0700

    Finished Saving Data portion, TODO: save and close images and add Batch processing

--- Fiji_scripts/Live-Dead_CellAnalysis/LDCell_Count.py
+++ Fiji_scripts/Live-Dead_CellAnalysis/LDCell_Count.py
@@ -13,11 +13,29 @@ from loci.plugins.in import ImporterOptions, ImagePlusReader, ImportProcess
 name = ['File']
 layer = ['Layer']
 split = 3
+results = ['Slice,Count,Total Area,Average Size,%Area']
+
+def Analyze(imp):
+	IJ.setAutoThreshold(imp, "Otsu dark stack")
+	IJ.run(imp, "Convert to Mask", "method=MaxEntropy background=Dark calculate black")
+	IJ.run(imp, "Despeckle", "stack")
+	IJ.run(imp, "Watershed", "stack")
+	IJ.run(imp, "Analyze Particles...", "size=8-Infinity pixel show=Masks summarize stack")
+
+def fileProcess(title):
+	#save Summary Window
+	IJ.selectWindow("Summary of " + title)	
+	tempFile = saveDir + "/" + title + str(i)+ ".csv"
+	IJ.saveAs("Results", tempFile)
+	#open data in string for final file save
+	with open(tempFile) as r:
+		result = r.read().splitlines()
+	return result[1:]
 
 # Get file name and path
 filename = file.getAbsolutePath()
+saveName = os.path.basename(filename)
 dirPath = os.path.dirname(filename)
-print os.path.realpath(filename)
 
 # Check if folders are there or not   
 if not os.path.exists(dirPath + '/Results'):     
@@ -58,37 +76,28 @@ for i in range(0, nseries):
 		imp.show()
 		# int array [width, height, nChannels, nSlices, nFrames]
 		Dim = imp.getDimensions()
+		print Dim
 
 		for i in range(split):
 			titles = [title]*Dim[2]*Dim[3]
 			name.extend(titles)
 			layer.extend([i+1]*Dim[2]*Dim[3])
 			
-			# Set up ROIs to analyze from (split into 1/3 portions)
+			# Set up ROIs and analyze based on how it is split
 			imp.setRoi(0, i*Dim[1]/3, Dim[0], Dim[1]/3)
-		
-			IJ.setAutoThreshold(imp, "Otsu dark stack")
-			IJ.run(imp, "Convert to Mask", "method=MaxEntropy background=Dark calculate black")
-			IJ.run(imp, "Despeckle", "stack")
-			IJ.run(imp, "Watershed", "stack")
-			IJ.run(imp, "Analyze Particles...", "size=8-Infinity pixel show=Masks summarize stack")
+			Analyze(imp)
+
 		opts.setSeriesOn(i, False)
 
 
-	# save results
+	# save results of series
 	saveDir = "C:/Users/Erikka Linn/Documents/Masuda Lab/Test"
-	IJ.selectWindow("Summary of " + title)	
-	IJ.saveAs("Results", saveDir + "/" + title + str(i)+ ".csv")
-	break;
+	results.extend(fileProcess(title))
 
 
 # concatenate all excel files into one file
-print name, layer
-with open(saveDir + '/names.csv', 'w') as f:
-	writer = csv.writer(f, delimiter=',')
-	writer.writerows(zip(name, layer))
-
-#os.system("cat " + saveDir + "/*.csv > C:/Users/Erikka Linn/Documents/Masuda Lab/Results.csv") 
+with open(saveDir + '/' + saveName + '.csv', 'w') as w:
+	w.write('\n'.join('%s,%s,%s' % x for x in zip(name, layer, results)))
 
 
 
