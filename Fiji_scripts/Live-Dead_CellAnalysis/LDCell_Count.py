96ee6d6269dc7c47ebfc65609b0f03a08f632bd5
Erikka <eeelinn@gmail.com>
2020-10-25 20:04:23 -0700

    updates

--- Fiji_scripts/Live-Dead_CellAnalysis/LDCell_Count.py
+++ Fiji_scripts/Live-Dead_CellAnalysis/LDCell_Count.py
@@ -1,34 +1,45 @@
 #@File (label='Choose file to analyze') file
 #@File(label='Choose a save directory', style='directory') save_Dir
-#@ Short(label='# of Slices', min=1) split
+#@Integer(label="remove first __ slices", value=5) front
+#@Integer(label="remove last __ slices", value=5) back
+#@Output string sumtext
 
 import os, csv
 from ij.gui import Roi, Overlay, TextRoi
 from ij.plugin import ChannelSplitter
 from java.awt import Font, Color
 from loci.plugins import BF
-from ij import IJ, ImageStack, WindowManager
+from ij import IJ, ImageStack, WindowManager, ImagePlus
 from loci.common import Region
 from loci.plugins.in import ImporterOptions, ImagePlusReader, ImportProcess
 
+
 def Analyze(imp):
-	IJ.setAutoThreshold(imp, "Otsu dark stack")
+	IJ.setAutoThreshold(imp, "Otsu Dark stack")
 	IJ.run(imp, "Convert to Mask", "method=MaxEntropy background=Dark calculate black")
+	#IJ.run(imp, "Invert", "stack")
 	IJ.run(imp, "Despeckle", "stack")
 	IJ.run(imp, "Watershed", "stack")
 	IJ.run(imp, "Analyze Particles...", "size=8-Infinity pixel show=Masks summarize stack")
 
-def saveImg(imp, nslices):
+def sliceCrop(stack, start, end):
+	for i in range(0, start):
+		stack.getStack().deleteSlice(1)
+	for i in range(0, end):
+		stack.getStack().deleteLastSlice()
+	return stack
+
+def saveImg(imp, nslices, label):
 	mask = WindowManager.getImage('Mask of ' + imp.getTitle())
-	IJ.save(mask, saveIm + '/' + title + '_' + str(i+1) + '.tif')
-	label = mask.duplicate()
+	IJ.save(mask, saveDir + '/' + title + '_' + label + '.tif')
+	labeled = mask.duplicate()
 	for n in range(1,nslices+1):
-		s = label.getStack().getProcessor(n);
+		s = labeled.getStack().getProcessor(n);
 		s.setFont(Font("SansSerif", Font.BOLD, 30));
 		s.setColor(Color.blue);
-		s.drawString(title + ' Layer: ' + str(i+1) + ' Slice: ' + str(n), 20, 40)
-	label.updateAndDraw()
-	IJ.save(label, saveIm + '/labeled_' + title + '_' + str(i+1) + '.tif')
+		s.drawString(title + ' Layer: ' + label + ' Slice: ' + str(n), 20, 40)
+	labeled.updateAndDraw()
+	IJ.save(labeled, saveDir + '/labeled_' + title + '_' + label + '.tif')
 	IJ.run("Close")
 
 def fileProcess(title):
@@ -36,30 +47,40 @@ def fileProcess(title):
 	IJ.selectWindow("Summary of " + title)	
 	tempFile = saveDir + "/temp_" + title + str(i)+ ".csv"
 	IJ.saveAs("Results", tempFile)
+	IJ.run("Close")
+	count = []
 	
-	#open data in string for final file save
+	# open data in string for final file save
 	with open(tempFile, 'r') as r:
 		result = r.read().splitlines()
-	return result[1:]
+		for n in range(1, len(result)):
+			array = result[n].split(',')
+			count.append(int(array[1]))
+
+	# delete temp file and return
+	os.remove(tempFile)
+	return result[1:], count
 	
 
 # Get file name and path
 filename = file.getAbsolutePath()
 saveName = os.path.basename(filename)
-saveDir = str(save_Dir) + '/Results'
-saveIm = str(save_Dir) + '/CountedImages'
+saveDir = str(save_Dir) + '/Results_' + saveName
+
+layers = [0.235, 0.315, 0.45]
+labels = ["Top", "Middle", "Bottom"]
 
 # Initialize variables for Saving
 name = ['File']
 layer = ['Layer']
 results = ['Slice,Count,Total Area,Average Size,%Area']
+summary = {"Top": [], "Middle": [], "Bottom": []}
+
 
 # Check if folders are there or not   
 if not os.path.exists(saveDir):     
 	os.mkdir(saveDir)
-if not os.path.exists(saveIm):    	
-	os.mkdir(saveIm)
-
+	
 # set up options for import
 opts = ImporterOptions()
 opts.setId(filename)
@@ -97,40 +118,54 @@ for i in range(0, nseries):
 
 	# run analysis on active series for all images in stack
 	for imp in imps:
-		#title = imp.getTitle()
-		#imp.show()
 		channels = splitter.split(imp)
 		
-		# int array [width, height, nChannels, nSlices, nFrames]
-		Dim = imp.getDimensions()
 		for c in channels:
 			title = c.getTitle()
-			for i in range(split):
+			c = sliceCrop(c, front, back)
+			
+			# int array [width, height, nChannels, nSlices, nFrames]
+			Dim = c.getDimensions()
+			prev = 0
+			for i in range(0, len(layers)):
 				titles = [title]*Dim[3]
 				name.extend(titles)
-				layer.extend([i+1]*Dim[3])
+				layer.extend([labels[i]]*Dim[3])
 			
 				# Set up ROIs and analyze based on how it is split
-				c.setRoi(0, i*Dim[1]/split, Dim[0], Dim[1]/split)
+				c.setRoi(0, int(prev*Dim[1]), Dim[0], int(layers[i]*Dim[1]))
 				Analyze(c)
-				saveImg(c, Dim[3])
+				saveImg(c, Dim[3], labels[i])
+				
+				# save results of series
+				prev = prev + layers[i]
+				all, count = fileProcess(title)
+				summary[labels[i]].append(sum(count))
+				results.extend(all)
 			
-			# save results of series
-			results.extend(fileProcess(title))
 
-#IJ.run("Close All")
-
+# prepare summary
+sumtext = 'Summary: {0}\nLayer,Live,Dead,Viability(%)\n'.format(saveName)
+tLive = 0.0
+tDead = 0.0
+for key in labels:
+	live = float(summary[key][0])
+	tLive += live
+	dead = float(summary[key][1])
+	tDead += dead
+	if live + dead == 0:
+		viability = 'NA'
+	else:
+		viability = live / (live + dead) * 100
+	sumtext += '{0},{1},{2},{3}\n'.format(key, live, dead, viability)
+sumtext += 'Total, {0},{1},{2}\n'.format(tLive, tDead, tLive/(tLive + tDead)*100)
+	
 # concatenate all excel files into one file
 with open(saveDir + '/' + saveName + '.csv', 'w') as w:
+	w.write(sumtext)
+	w.write('\nRaw Data\n')
 	w.write('\n'.join('%s,%s,%s' % x for x in zip(name, layer, results)))
 
-# Remove temporary .csv files
-for fname in os.listdir(saveDir):
-    if fname.startswith("temp_"):
-        os.remove(os.path.join(saveDir, fname))
-
-
-#TODO: close summary table
-
-
-
+# add to the log file
+with open(str(save_Dir) + '/summary.csv', 'a+') as s:
+	s.write(sumtext)
